e355fc06827f2d27836d3ace6719639569142946
pea5e <sindibade2011@gmail.com>
2024-06-27 06:48:26 +0100

    push

--- FlightApp.API/Controllers/Checkings.cs
+++ FlightApp.API/Controllers/Checkings.cs
@@ -1,8 +1,12 @@
 ﻿using FlightApp.Core;
 using FlightApp.Core.Models;
 using FlightApp.Core.Repositories;
+using FlightApp.Data.Migrations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using static System.Net.WebRequestMethods;
 
 namespace FlightApp.API.Controllers
 {
@@ -22,5 +26,22 @@ namespace FlightApp.API.Controllers
         {
             return Ok(_contextAccessor.Checkings.findById(1));
         }
+
+
+        [HttpGet]
+        [Route("destinations/{airport}")]
+        public async Task<IActionResult> GetDestinations(string airport)
+        {
+            using (var client = new HttpClient())
+            {
+                string uri = "https://pea5e.pythonanywhere.com/departure/";
+                var result = await client.GetStringAsync(uri + airport);
+
+                List<Flight> flights = JsonConvert.DeserializeObject<List<Flight>>(result);
+                return Ok(flights);
+            }
+            return Ok(new List<Flight>());
+        }
+        
     }
 }
